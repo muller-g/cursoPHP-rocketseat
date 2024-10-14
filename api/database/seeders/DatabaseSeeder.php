ab8dc48ce244418773de1745618f5cc0d4ced968
muller-g <gabriel_muller98@hotmail.com>
2024-10-13 23:13:04 -0300

    feat: aula 3 finalizada

--- api/database/seeders/DatabaseSeeder.php
+++ api/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Actions\ArrangePositions;
 use App\Models\Project;
 use App\Models\Proposal;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -17,7 +18,9 @@ class DatabaseSeeder extends Seeder
         User::query()->inRandomOrder()->limit(10)->get()
             ->each(function (User $u) {
                 $project = Project::factory()->create(['created_by' => $u->id]);
-                Proposal::factory()->count(random_int(4, 45))->create(['project_id' => $project->id]);
+                Proposal::factory()->count(random_int(1, 45))->create(['project_id' => $project->id]);
+
+                ArrangePositions::run($project->id);
             });
     }
 }
